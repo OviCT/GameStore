e973c409a122467ef0e28e57fec82661fb9966d5
Cătăramă Ovidiu <catarama.ovidiu66@gmail.com>
2018-09-02 09:50:18 +0300

    update menu.php

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <?php include 'style/import-style.php'; ?>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
+<nav class="navbar navbar-expand-lg navbar-light navbar-light" style="background-color: #ff99cc;">
   <a class="navbar-brand" href="#">Game Store</a>
   <!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
